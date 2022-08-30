919b84b9af9d6119211b9163d45e0c79d1c17f1f
federicodiaz1 <federico-agustin-diaz>
2022-08-29 22:03:04 -0300

    remove

--- Caren_RN_20220314/hooks/useAppTheme.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import ThemeContext from '../ThemeContext';
-
-export default () => {
-  const appTheme = React.useContext(ThemeContext);
-  return appTheme;
-};
--- Caren_RN_20220314/hooks/useAuth.ts
+++
@@ -1,8 +0,0 @@
-import {AuthContext} from '../AuthContext';
-import React from 'react';
-
-export default () => {
-  const {isInitialized, isSignedIn, isIntro, signIn, signOut} =
-    React.useContext(AuthContext);
-  return {isInitialized, isSignedIn, isIntro, signIn, signOut};
-};
